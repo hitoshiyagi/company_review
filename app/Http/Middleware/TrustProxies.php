0c8bacf45438224c02cca9120f61135d23536492
yagi-web <223397855+yagi-web@users.noreply.github.com>
2026-01-03 13:54:51 +0900

    シンプル構造

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,23 +7,12 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * このアプリを信頼するプロキシ（Heroku）の設定
-     *
-     * @var array<int, string>|string|null
-     */
-    // ★ここを null や array から '*' に書き換えます
+    // app/Http/Middleware/TrustProxies.php の中身
+
     protected $proxies = '*';
 
-    /**
-     * 使用するヘッダーの設定
-     *
-     * @var int
-     */
-    protected $headers =
-    Request::HEADER_X_FORWARDED_FOR |
+    protected $headers = Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_AWS_ELB;
+        Request::HEADER_X_FORWARDED_PROTO;
 }
