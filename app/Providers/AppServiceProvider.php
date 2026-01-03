3faad561fb831e96772d7077adf6183dca7793d6
yagi-web <223397855+yagi-web@users.noreply.github.com>
2026-01-03 11:11:47 +0900

    heriku対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-     if(\App::environment(['priduction'])){
+     if(\App::environment(['production'])){
         \URL::forceScheme('https');
      }
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ asset('vendor/adminlte/dist/img/logo.png') }}" alt="Logo" style="width: 120px; height: auto;">
+                    <img src="{{ secure_asset('vendor/adminlte/dist/img/logo.png') }}" alt="Logo" style="width: 120px; height: auto;">
 
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
